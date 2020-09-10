3f1153b5d34c445b8be8506976a0eee3caf127c8
aadhityasw <aadhityas@gmail.com>
2020-09-10 09:04:16 +0530

    adds a fully function serial api call feature
    UI has not been built

--- DonorMatch/views.py
+++ DonorMatch/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+import requests
 
 # Create your views here.
 
@@ -12,9 +15,7 @@ def DonorSearch(request) :
     if request.method == 'GET' :
         return render(request, 'search.html', {'display_data':False})
     else :
-        context = {'display_data':True}
-
-        # Recieve data from form
+        # Recieve data from the form
         blood_abo = request.POST['abo']
         blood_rh = request.POST['rh']
         city = request.POST['city']
@@ -22,5 +23,26 @@ def DonorSearch(request) :
         state = request.POST['state']
 
         # Perform api calls here
+        url = "http://127.0.0.1:8000/api/patient_details/abo=" + blood_abo + "&rh=" + blood_rh + "&city=" + (" " if city == "" else city) + "&district=" + (" " if district == "" else district) + "&state=" + (" " if state == "" else state)
+        response = requests.get(url).json()
+        patient_objects = []
+        for i in range(len(response)) :
+            current_data = response[i]
+            current_data["hospital"] = "Plasm_Hospital 1"
+            data_arr = [
+                current_data["blood_abo_type"],
+                current_data["blood_rh_type"],
+                "Plasm_Hospital 1",
+                "Recovered",
+                current_data["age_bracket"],
+                current_data["gender"],
+                current_data["nationality"],
+                current_data["city"]
+            ]
+            patient_objects.append(data_arr)
 
+
+        # Return the values to the web page
+
+        context = {'display_data':True, 'patient_data':patient_objects, 'num_patients':len(patient_objects)}
         return render(request, 'search.html', context)
