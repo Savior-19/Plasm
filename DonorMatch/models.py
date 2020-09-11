a9432fed0cf176f19e76075f23125dac42c71891
aadhityasw <aadhityas@gmail.com>
2020-09-11 23:47:27 +0530

    adds an api table and functioning thread level parallelism

--- DonorMatch/models.py
+++ DonorMatch/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class APICalls(models.Model) :
+    hospital_name = models.CharField(max_length=100, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    api_url = models.CharField(null=False, blank=False, max_length=500)
+
+    def __str__(self):
+        return (self.hospital_name)
--- DonorMatch/views.py
+++ DonorMatch/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import APICalls
+
 import requests
+import threading
+import string
+import math
 
 # Create your views here.
 
@@ -10,39 +15,85 @@ def HomePage(request) :
     return render(request, 'home.html')
 
 
-#@login_required
-def DonorSearch(request) :
-    if request.method == 'GET' :
-        return render(request, 'search.html', {'display_data':False})
-    else :
-        # Recieve data from the form
-        blood_abo = request.POST['abo']
-        blood_rh = request.POST['rh']
-        city = request.POST['city']
-        district = request.POST['district']
-        state = request.POST['state']
+patient_data = []
+patient_data_lock = threading.Lock()
 
-        # Perform api calls here
-        url = "http://127.0.0.1:8000/api/patient_details/abo=" + blood_abo + "&rh=" + blood_rh + "&city=" + (" " if city == "" else city) + "&district=" + (" " if district == "" else district) + "&state=" + (" " if state == "" else state)
+
+def serial_task(hospitals, blood_requirement) :
+    """ Performs the serial task of making the api calls and storing the information in an array. """
+    global patient_data
+    patient_data_thread_local = []
+    for hospital in hospitals :
+        url = hospital.api_url
+        url = string.Template(url).substitute(**blood_requirement)
         response = requests.get(url).json()
-        patient_objects = []
         for i in range(len(response)) :
             current_data = response[i]
             current_data["hospital"] = "Plasm_Hospital 1"
             data_arr = [
                 current_data["blood_abo_type"],
                 current_data["blood_rh_type"],
-                "Plasm_Hospital 1",
+                hospital.hospital_name,
                 "Recovered",
                 current_data["age_bracket"],
                 current_data["gender"],
                 current_data["nationality"],
                 current_data["city"]
             ]
-            patient_objects.append(data_arr)
+            patient_data_thread_local.append(data_arr)
+    
+    # Acquire the lock
+    patient_data_lock.acquire()
+    # Make the additions to the main list of patient data
+    patient_data = patient_data + patient_data_thread_local
+    # Release the lock
+    patient_data_lock.release()
 
+
+#@login_required
+def DonorSearch(request) :
+    if request.method == 'GET' :
+        return render(request, 'search.html', {'display_data':False})
+    else :
+        global patient_data
+
+        # Recieve data from the html-form
+        blood_requirement = {
+            'blood_abo' : request.POST['abo'],
+            'blood_rh' : request.POST['rh'],
+            'city' : " " if request.POST['city'] == "" else request.POST['city'],
+            'district' : " " if request.POST['district'] == "" else request.POST['district'],
+            'state' : " " if request.POST['district'] == "" else request.POST['district']
+        }
+
+        
+        # Perform api calls here
+
+        # Get the hospital details from the database
+        hospital_details = APICalls.objects.all()
+        num_threads = 2
+        thread_job_pool = []
+        thread_max_num_jobs = math.ceil(len(hospital_details) / num_threads)
+        for i in range(num_threads) :
+            thread_job_pool.append(hospital_details[(i * thread_max_num_jobs) : min(((i+1) * thread_max_num_jobs), len(hospital_details))])
+        print(thread_job_pool)
+
+        # Sample URL Pattern
+        #url = "http://127.0.0.1:8000/api/patient_details/abo=$blood_abo&rh=$blood_rh&city=$city&district=$district&state=$state"
+        #url = "http://127.0.0.1:8000/api/patient_details/abo=" + blood_abo + "&rh=" + blood_rh + "&city=" + (" " if city == "" else city) + "&district=" + (" " if district == "" else district) + "&state=" + (" " if state == "" else state)
+        
+
+        # Threading setup
+        threads = []
+        for i in range(num_threads-1) :
+            t = threading.Thread(target=serial_task, args=(thread_job_pool[i], blood_requirement))
+            threads.append(t)
+            t.start()
+        serial_task(thread_job_pool[-1], blood_requirement)
+        for i in range(num_threads-1) :
+            threads[i].join()
 
         # Return the values to the web page
 
-        context = {'display_data':True, 'patient_data':patient_objects, 'num_patients':len(patient_objects)}
+        context = {'display_data':True, 'patient_data':patient_data, 'num_patients':len(patient_data)}
         return render(request, 'search.html', context)
