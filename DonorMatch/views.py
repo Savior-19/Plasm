52b18e1a664c9194dde20985d13fc816b46e3012
aadhityasw <aadhityas@gmail.com>
2020-10-29 12:13:22 +0530

    adds functionality for measuring time

--- DonorMatch/views.py
+++ DonorMatch/views.py
@@ -7,6 +7,7 @@ import requests
 import threading
 import string
 import math
+import time
 
 # Create your views here.
 
@@ -74,6 +75,8 @@ def DonorSearch(request) :
         hospital_details = APICalls.objects.all()
         num_threads = 2
         thread_job_pool = []
+        # Start of the parallel execution
+        parallel_execution_start = time.time()
         thread_max_num_jobs = math.ceil(len(hospital_details) / num_threads)
         for i in range(num_threads) :
             thread_job_pool.append(hospital_details[(i * thread_max_num_jobs) : min(((i+1) * thread_max_num_jobs), len(hospital_details))])
@@ -85,16 +88,63 @@ def DonorSearch(request) :
         
 
         # Threading setup
+        execution_start_time = time.time()
         threads = []
         for i in range(num_threads-1) :
+            # Execution of the serial task
             t = threading.Thread(target=serial_task, args=(thread_job_pool[i], blood_requirement))
             threads.append(t)
             t.start()
+        # Execution of the serial task for the master thread
         serial_task(thread_job_pool[-1], blood_requirement)
+        execution_end_time = time.time()
         for i in range(num_threads-1) :
             threads[i].join()
+        parallel_execution_end = time.time()
+
+        # Calculate the time taken
+        #print("Time taken for the total parallel execution : ", (parallel_execution_end - parallel_execution_start))
+        print("Time taken for the parallel execution alone : ", (execution_end_time - execution_start_time))
+        print("Time taken for the setup process for the threads (the additional overhead due to threading) : ", (execution_start_time - parallel_execution_start))
+
+        # """
+        # In case we need to demonstrate the serial execution
+        serial_execution_start = time.time()
+        parform_task_serially(hospital_details, blood_requirement)
+        serial_execution_end = time.time()
+
+        print("TIme taken for Serial Execution : ", (serial_execution_end - serial_execution_start))
+        # """
+
 
         # Return the values to the web page
-
         context = {'display_data':True, 'patient_data':patient_data, 'num_patients':len(patient_data)}
         return render(request, 'search.html', context)
+
+
+
+
+def parform_task_serially(hospitals, blood_requirement) :
+    """
+        Performs the serial task of making the api calls and storing the information in an array.
+        This is done in a serial manner without using threads.
+    """
+    patient_data = []
+    for hospital in hospitals :
+        url = hospital.api_url
+        url = string.Template(url).substitute(**blood_requirement)
+        response = requests.get(url).json()
+        for i in range(len(response)) :
+            current_data = response[i]
+            current_data["hospital"] = "Plasm_Hospital 1"
+            data_arr = [
+                current_data["blood_abo_type"],
+                current_data["blood_rh_type"],
+                hospital.hospital_name,
+                "Recovered",
+                current_data["age_bracket"],
+                current_data["gender"],
+                current_data["nationality"],
+                current_data["city"]
+            ]
+            patient_data.append(data_arr)
